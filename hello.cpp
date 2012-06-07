d7a56417186ca746a6c4838b2400198364678a8a
lbragin <return_to_castle@mail.ru>
2012-06-07 14:01:26 +0600

    Программа на C++.

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char name[80];
+  count << "Enter your name: ";
+  cin >> name;
+  cout << "Hello, " << name << " :)";
+  char ch;
+  cin >> ch;
+  return 0;
+}
